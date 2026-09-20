707018b79aa7b79a451a12cb77f0c57d2ce79e74
Micheal Onye <monyemelonu@gmail.com>
2026-09-21 00:17:33 +0200

    feat: populate FeatureCollection object with teh right properties

--- week03/code/FeatureCollection.cs
+++ week03/code/FeatureCollection.cs
@@ -1,5 +1,18 @@
 public class FeatureCollection
 {
-    // TODO Problem 5 - ADD YOUR CODE HERE
+    public Feature[] Features { get; set; } = [];
     // Create additional classes as necessary
+}
+
+
+public class Feature
+{
+    public EarthquakeProperties Properties { get; set; } = new();
+    // Create additional classes as necessary
+}
+
+public class EarthquakeProperties
+{
+    public string Place { get; set; } = "";
+    public double Mag { get; set; } 
 }
